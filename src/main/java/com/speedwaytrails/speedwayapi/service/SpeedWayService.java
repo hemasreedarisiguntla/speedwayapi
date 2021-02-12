673705c299b4de01df94134078dd9f46b5368902
Hemasree Darisiguntla <sreed2503@gmail.com>
2021-02-11 19:00:59 -0500

    Review comment fix to initialize data in test

--- src/main/java/com/speedwaytrails/speedwayapi/service/SpeedWayService.java
+++ src/main/java/com/speedwaytrails/speedwayapi/service/SpeedWayService.java
@@ -17,14 +17,6 @@ public class SpeedWayService {
         this.raceCarRepository = raceCarRepository;
     }
 
-    @PostConstruct
-    public void init(){
-        RaceCar raceCar = RaceCar.builder().nickname("The Condor").model("Corvette").year("2010").status("AVAILABLE").top_speed(189).build();
-        Driver driver = Driver.builder().first_name("jack").last_name("dan").age(32).nickname("ja").wins(1).losses(1).build();
-        raceCar.getDriverList().add(driver);
-        raceCarRepository.save(raceCar);
-
-    }
 
     //Browse the racecars to get details about them.
     public List<RaceCar> getAllRaceCars() {
